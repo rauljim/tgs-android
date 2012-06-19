e127dadbb094aae655b77bdf8f01ab5d5b5894a5
Raul Jimenez <rauljc@kth.se>
2012-06-19 17:21:20 +0200

    Added Intro checkbox

--- src/com/tudelft/triblerdroid/first/IntroActivity.java
+++ src/com/tudelft/triblerdroid/first/IntroActivity.java
@@ -3,27 +3,52 @@ package com.tudelft.triblerdroid.first;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class IntroActivity extends Activity {
-	Button b_continue;
+    public static final String PREFS_NAME = "settings.dat";
+    Button b_continue;
+    CheckBox cb_showIntro;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);	  
-	  
-		setContentView(R.layout.intro);
-		b_continue = (Button) findViewById(R.id.b_continue);
-		b_continue.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				setContentView(R.layout.pythonautoinstall);
-				Intent intent = new Intent(getBaseContext(), PythonAutoinstallActivity.class);
-				startActivityForResult(intent, 0);
-			}  	
-		});
+		
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		boolean showIntro = settings.getBoolean("showIntro", true);
+
+		if (showIntro) {
+			setContentView(R.layout.intro);
+			cb_showIntro = (CheckBox) findViewById(R.id.cb_show_intro);
+			cb_showIntro.setChecked(true);
+			b_continue = (Button) findViewById(R.id.b_continue);
+			b_continue.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					if (!cb_showIntro.isChecked()){
+						SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+						SharedPreferences.Editor editor = settings.edit();
+						editor.putBoolean("showIntro", false);
+						editor.commit(); //Raul: don't forget to commit edits!!
+						Log.w("intro", "Don't show Intro next time");
+					}
+					setContentView(R.layout.pythonautoinstall);
+					Intent intent = new Intent(getBaseContext(), PythonAutoinstallActivity.class);
+					startActivityForResult(intent, 0);
+				}  	
+			});
+		}
+		else
+		{
+			Log.w("intro", "don't show intro: go to P2P directly");
+			Intent intent = new Intent(getBaseContext(), P2PStartActivity.class);
+			startActivityForResult(intent, 0);
+		}
 	}
 	
 	@Override
