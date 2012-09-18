e5e629890b4e191deb00f41ef0be1eabd1d26c46
Raul Jimenez <rauljc@kth.se>
2012-09-18 14:32:08 +0200

    working on Twitter posting

--- src/com/tudelft/triblerdroid/first/Preferences.java
+++ src/com/tudelft/triblerdroid/first/Preferences.java
@@ -1,5 +1,7 @@
 package com.tudelft.triblerdroid.first;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
@@ -27,5 +29,28 @@ public class Preferences extends PreferenceActivity {
                 return true;
             }
         });
+		button = (Preference)findPreference("pref_tweet");
+		button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference arg0) { 
+            	String tweetUrl = "https://twitter.com/intent/tweet?text=" +
+            			"Video description ppsp.me/2b2fe5f1462e5b7ac4d70fa081e0169160b2d3a6";
+            	Uri uri = Uri.parse(tweetUrl);
+            	startActivity(new Intent(Intent.ACTION_VIEW, uri));
+                return true;
+            }
+        });
+		button = (Preference)findPreference("pref_share");
+		button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference arg0) { 
+            	 Intent share = new Intent(Intent.ACTION_SEND);
+            	 share.putExtra(Intent.EXTRA_TEXT, 
+            			 "Video description ppsp.me/2b2fe5f1462e5b7ac4d70fa081e0169160b2d3a6");
+            	 startActivity(Intent.createChooser(share, "Share this via"));
+                return true;
+            }
+        });
+		
 	}
 }
