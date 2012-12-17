cf28378d20436956df577b1046fdfab6d44a2575
Raul Jimenez <rauljc@kth.se>
2012-12-17 13:28:42 +0100

    cleaning up warnings: removed unused imports

--- src/com/tudelft/triblerdroid/first/LiveIPDialogFragment.java
+++ src/com/tudelft/triblerdroid/first/LiveIPDialogFragment.java
@@ -1,16 +1,14 @@
 package com.tudelft.triblerdroid.first;
 
 import android.app.Activity;
-import android.app.Dialog;
 import android.app.AlertDialog;
-import android.support.v4.app.DialogFragment;
-import android.view.View;
-import android.view.LayoutInflater;
-import android.widget.EditText;
+import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 import me.ppsp.test.R;
 
 public class LiveIPDialogFragment extends DialogFragment {
